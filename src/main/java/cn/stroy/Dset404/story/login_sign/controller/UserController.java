36d07cdd33b9ad7f0469e16a7901bf08928b67bd
e <destinyhao@foxmail.com>
2017-10-03 13:57:28 +0800

    update UEditor

--- src/main/java/cn/stroy/Dset404/story/login_sign/controller/UserController.java
+++ src/main/java/cn/stroy/Dset404/story/login_sign/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
 
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public @ResponseBody List<Story_user> findAll() {
-        System.out.println("findAll- - - test - - -" );
+        System.out.println("findAll- - - - - -" );
         return userService.findAll();
     }
 
